936495050ab0e1744e8e6d6a38e684d76e42793c
Ajay C M <ajaymendigeri@gmail.com>
2026-04-20 04:42:45 +0000

    Array

---
+++ Arrays/A1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int size;
+    printf("Enter the size: ");
+    scanf("%d",&size);
+
+    int arr[size];
+    printf("Enter the array elements: ");
+    for(int i=0; i<size; i++){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Array elements: ");
+    for(int i=0; i<size; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
