6dec668718ec46cf9ea99f396acdf1c442aa0369
Ajay C M <ajaymendigeri@gmail.com>
2026-04-22 17:45:59 +0000

    rev

---
+++ Patterns/p3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num=0;
+    printf("Enter the size: ");
+    scanf("%d",&num);
+    for(int i=1; i<=num;i++){
+        for(int j=1; j<=num-i+1;j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
