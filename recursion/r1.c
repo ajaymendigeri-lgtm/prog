5e6d095a4ef65f2c357706d7bb6b3073a1d2c210
Ajay C M <ajaymendigeri@gmail.com>
2026-04-29 19:07:59 +0000

    fact

---
+++ recursion/r1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int factorial(int n){
+    if(n==1) return 1;
+    return n*factorial(n-1);
+
+}
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    int a=factorial(n);
+    printf("%d\n",a);
+}
