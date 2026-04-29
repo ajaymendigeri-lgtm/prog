1dedd337b5aef199d27da3968d273260fbc0ebda
Ajay C M <ajaymendigeri@gmail.com>
2026-04-29 19:23:41 +0000

    n-1

---
+++ recursion/r2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int dec(int n){
+    if(n>0){ 
+    printf("%d\n",n);
+    dec(n-1);
+    return 0;
+    }
+    
+}
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    dec(n);
+}
