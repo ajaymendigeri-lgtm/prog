4795804ea8b62ca0a297b41ec3ad05292dfbcf85
Ajay C M <ajaymendigeri@gmail.com>
2026-05-01 18:42:15 +0000

    path

--- recursion/r5.c
+++ recursion/r5.c
@@ -19,3 +19,43 @@ int main(){
         fibonacci(limit, 0, 1);
     }
 }
+
+
+
+/*
+#include <stdio.h>
+
+// Print negative Fibonacci (left side)
+void negafib(int limit, int a, int b){
+    if(a < limit) return;
+
+    negafib(limit, b, a - b);  // go deeper first (to reverse order)
+    printf("%d ", a);
+}
+
+// Print positive Fibonacci (right side)
+void posfib(int limit, int a, int b){
+    if(a > limit) return;
+
+    printf("%d ", a);
+    posfib(limit, b, a + b);
+}
+
+int main(){
+    int limit;
+
+    printf("Enter the limit: ");
+    scanf("%d", &limit);
+
+    if(limit < 0){
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    // Negative side (excluding 0)
+    negafib(-limit, 0, 1);
+
+    // Positive side (including 0)
+    posfib(limit, 0, 1);
+}
+*/
---
+++ recursion/stairpath.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int stair(int n){
+    if(n==1 || n==2) return n;
+
+    int totalways=stair(n-1)+stair(n-2);
+    return totalways;
+}
+
+
+
+
+
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    int ans=stair(n);
+    printf("%d",ans);
+}
